6df4c1bfe2ceea101743d29aca708209f61e94d0
KATIETAG <64670675+KATIETAG@users.noreply.github.com>
2023-08-15 12:36:57 -0700

    Implemented the Edit button

--- gui.py
+++ gui.py
@@ -6,20 +6,40 @@ label = sg.Text("Type in a to-do")
 # key will change the key from number to the text we identify
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 # if you add items in 1 list : they will be in 1 row layout=[[label, input_box]]
 #  layout=[[label], [input_box]] will show in 2 rows
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
-    print("Hello")
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     if event == "Add":
         todos = functions.get_todos()
         new_todo = values['todo'] + "\n"
         todos.append(new_todo)
         functions.write_todos(todos)
+        #refresh todo list instantly
+        window['todos'].update(values=todos)
+    if event == "Edit":
+        todo_to_edit = values['todos'][0]
+        new_todo = values['todo'] +"\n"
+
+        todos = functions.get_todos()
+        index = todos.index(todo_to_edit)
+        todos[index] = new_todo
+        functions.write_todos(todos)
+        # refresh window in real time
+        window['todos'].update(values=todos)
+        #put current selection in text box
+    if event == 'todos':
+        window['todo'].update(value=values['todos'][0])
     if event == sg.WIN_CLOSED:
         break
 
